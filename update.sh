ed0bbc3b1fc00538cfb1f03e8319e5db1630c883
zhiwenwang20122302 <zhiwenwang20122302@gamil.com>
2020-03-23 10:44:11 +0800

    update 2020-03-23,10-03-1584931451

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+cur_date="`date +%Y-%m-%d,%H-%m-%s`"
 git add .
-git commit -m "update "
+git commit -m "update ${cur_date}"
 git remote add origin https://github.com/zhiwenwang20122302/Online-video-course.git
 git push -u origin master
